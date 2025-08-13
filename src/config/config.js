a39b04ae8118cf307c2cdb45a8ddb7d9023cf70f
Saidatou <babasaidatou@yahoo.fr>
2025-08-13 15:24:15 +0200

    config.js changes

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,4 @@
-const API_BASE_URL = 'https://next-laravel-backend.sabt.fr/api';
-const IMAGE_BASE_URL = 'https://next-laravel-backend.sabt.fr';
+
 // const API_BASE_URL = 'http://127.0.0.1:8000/api';
 // const IMAGE_BASE_URL = 'http://127.0.0.1:8000';
 
